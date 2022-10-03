2a77455d8cac43d1b4355eb9c410089e3edd61ee
Austin Barrow <austinjbarrow2000@gmail.com>
2022-10-03 13:09:06 -0400

    Add operate function

---
+++ script.js
@@ -0,0 +1,13 @@
+function add(a,b) { return a + b; }
+function subtract(a,b) { return a - b; }
+function multiply(a,b) { return a * b; }
+function divide(a,b) { return a / b; }
+
+function operate(operand, a, b) {
+    switch(operand) {
+        case '+': return add(a,b);
+        case '-': return subtract(a,b);
+        case '*': return multiply(a,b);
+        case '/': return divide(a,b);
+    }
+}
