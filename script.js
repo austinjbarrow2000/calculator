c6c77a7818b9c4b176ef6f333fe1993956601603
Austin Barrow <austinjbarrow2000@gmail.com>
2022-10-03 20:39:43 -0400

    Add update display upon number press

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+
+var calculatorDisplayValue = '0';
+updateDisplay();
+
+const buttons = document.querySelector("#buttons");
+const numbers = buttons.querySelectorAll("button:not(.operand):not(.special)");
+
+numbers.forEach(number => number.addEventListener('click', function(e) {
+    calculatorDisplayValue = parseInt(calculatorDisplayValue + e.target.innerText);
+    updateDisplay();
+}));
+
+function updateDisplay() {
+    const display = document.querySelector('#display');
+
+    display.innerHTML = calculatorDisplayValue;
+}
+
 function add(a,b) { return a + b; }
 function subtract(a,b) { return a - b; }
 function multiply(a,b) { return a * b; }
